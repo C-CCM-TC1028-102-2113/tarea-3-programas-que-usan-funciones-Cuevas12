99be372bc68f13fcf30d3508aa061164cfeb5a70
Cuevas12 <89288980+Cuevas12@users.noreply.github.com>
2021-08-31 11:22:16 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,18 @@
+def es_bisiesto(año):
+    if año % 400 == 0:
+        print('True', es_bisiesto(año))
+    elif año % 100 == 0:
+        print ('False', es_bisiesto(año))
+    elif año % 4 == 0:
+        print ('True', es_bisiesto(año))
+    else:
+        print ('False', es_bisiesto(año))
+    return es_bisiesto(año)
 
 def main():
-    #escribe tu código abajo de esta línea
-    pass
+    año=float(input(''))
+    print(str(es_bisiesto(año)))
+    
 
-if __name__=='__main__':
-    main()
+    if __name__ == '__main__':
+        main()
